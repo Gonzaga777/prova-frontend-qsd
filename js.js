de5e2115ee723a7934094217704ebd6a814cfdfc
Gabriel <gabrieloliveiragonzaga1@hotmail.com>
2021-11-22 12:58:45 -0300

    Implementação do Regex e complemento das paginas

--- js.js
+++ js.js
@@ -1,9 +1,43 @@
-(function($){"use strict";$('.input100').each(function(){$(this).on('blur',function(){if($(this).val().trim()!=""){$(this).addClass('has-val');}
-else{$(this).removeClass('has-val');}})})
-var input=$('.validate-input .input100');$('.validate-form').on('submit',function(){var check=true;for(var i=0;i<input.length;i++){if(validate(input[i])==false){showValidate(input[i]);check=false;}}
-return check;});$('.validate-form .input100').each(function(){$(this).focus(function(){hideValidate(this);});});function validate(input){if($(input).attr('type')=='email'||$(input).attr('name')=='email'){if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/)==null){return false;}}
-else{if($(input).val().trim()==''){return false;}}}
-function showValidate(input){var thisAlert=$(input).parent();$(thisAlert).addClass('alert-validate');}
-function hideValidate(input){var thisAlert=$(input).parent();$(thisAlert).removeClass('alert-validate');}
-var showPass=0;$('.btn-show-pass').on('click',function(){if(showPass==0){$(this).next('input').attr('type','text');$(this).find('i').removeClass('zmdi-eye');$(this).find('i').addClass('zmdi-eye-off');showPass=1;}
-else{$(this).next('input').attr('type','password');$(this).find('i').addClass('zmdi-eye');$(this).find('i').removeClass('zmdi-eye-off');showPass=0;}});})(jQuery);
+let iptEmail = document.getElementById("email");
+iptEmail.onblur = function() {
+    var regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    let msgEmail = document.querySelector(".msg-email");
+    let msgEmailOk = document.querySelector(".msg-email.ok");
+
+    if (regexMail.test(iptEmail.value)) {
+        msgEmail.style.display = "none";
+        msgEmailOk.style.display = "block";
+    } else {
+        msgEmailOk.style.display = "none";
+        msgEmail.style.display = "block";
+    }
+}
+let iptCpf = document.getElementById("cpf");
+iptCpf.onblur = function() {
+    let regexCpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+    let msgCpfError = document.querySelector(".msg-cpf.msg-error");
+    let msgCpfOK = document.querySelector(".msg-cpf.msg-success");
+
+    if (regexCpf.test(iptCpf.value)) {
+        msgCpfError.style.display = "none";
+        msgCpfOK.style.display = "block";
+    } else {
+        msgCpfOK.style.display = "none";
+        msgCpfError.style.display = "block";
+    }
+}
+let iptPassword = document.getElementById("password")
+iptPassword.onblur = function () {
+    let regexPassword = (?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$";
+}
+
+let iptPassword = document.getElementById("password")
+let iptPassword = document.getElementById("password2")
+if (password == password2) {}
+    alert("Senha Correta")
+    else alert("Senha Incorreta , Digite novamente.")
+}
+let iptNumer = document.getElementById("numero");
+iptNumer.onblur = function() {
+    let regexNumber = regex = new Regex(@"^-?[0-9][0-9,\.]+$") ;
+} 
